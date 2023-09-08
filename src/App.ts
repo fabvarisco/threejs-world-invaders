06e06bb3ab5e438286a7d9b59e092333612ce007
Fabricio Varisco <fabricio.varisco@outlook.com>
2023-09-08 15:56:04 -0300

    Update App.ts

--- src/App.ts
+++ src/App.ts
@@ -2,17 +2,23 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { ARButton } from 'three/addons/webxr/ARButton.js';
 
+
 class App {
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private renderer: THREE.WebGLRenderer;
   private controls: OrbitControls;
-  private geometry: THREE.BufferGeometry;
-  private meshes: THREE.Mesh[];
-
+  private reticle: THREE.Mesh;
+  private hitTestSource: any;
+  private hitTestSourceRequested: boolean;
   constructor() {
+    this.hitTestSource = undefined;
+    this.hitTestSourceRequested = false;
 
-    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.set(0, 2, 5);
+    this.camera.lookAt(new THREE.Vector3(0, 0, 0))
 
     this.scene = new THREE.Scene();
 
@@ -35,37 +41,115 @@ class App {
     this.controls.update();
 
 
-    this.geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
-    this.meshes = [];
+    this.reticle = new THREE.Mesh(new THREE.RingGeometry(0.15, .2, 32).rotateX(-Math.PI / 2), new THREE.MeshStandardMaterial());
+    this.reticle.matrixAutoUpdate = false;
+    this.reticle.visible = false;
+    this.scene.add(this.reticle);
 
     window.addEventListener('resize', this.resize.bind(this));
   }
 
+
+private createInsideBox(): THREE.Object3D{
+  const geo = new THREE.BoxGeometry(1, 1, 1)
+  const mat = new THREE.MeshLambertMaterial({
+    color: 0xff0000,
+  })
+  const insideMat = new THREE.MeshLambertMaterial({
+    color: 0xffffff,
+    side: THREE.BackSide,
+  })
+  const box = new THREE.Mesh(geo, mat)
+  box.scale.setScalar(0.2)
+  box.rotation.x = Math.PI / 4
+  box.rotation.y = Math.PI / 4
+  const innerBox = new THREE.Mesh(geo, insideMat)
+  const insideBox = new THREE.Object3D()
+  insideBox.add(box)
+  insideBox.add(innerBox)
+  return insideBox
+}
+
+
+private createOutsideBox():THREE.Object3D {
+  const outsidePlaneGeo = new THREE.PlaneGeometry(1, 1, 1, 1)
+  const planeOptionArr = [
+    {// Right
+      rotateY: Math.PI / 2,
+      rotateX: 0,
+      translate: [0.5, 0, 0]
+    },
+    {// Left
+      rotateY: -Math.PI / 2,
+      rotateX: 0,
+      translate: [-0.5, 0, 0]
+    },
+    {// Top
+      rotateY: 0,
+      rotateX: -Math.PI / 2,
+      translate: [0, 0.5, 0]
+    },
+    {// Bottom
+      rotateY: 0,
+      rotateX: Math.PI / 2,
+      translate: [0, -0.5, 0]
+    },
+    {// Back
+      rotateY: 0,
+      rotateX: Math.PI,
+      translate: [0, 0, -0.5]
+    },
+  ]
+  const outsideMat = new THREE.MeshBasicMaterial({
+    colorWrite: false,
+  })
+  const outsideBox = new THREE.Object3D()
+  planeOptionArr.forEach((opt) => {
+    const { rotateY, rotateX, translate } = opt
+    const geo = outsidePlaneGeo.clone().rotateY(rotateY).rotateX(rotateX).translate(...translate)
+    const mesh = new THREE.Mesh(geo, outsideMat)
+    mesh.renderOrder = -1
+    outsideBox.add(mesh)
+  })
+  return outsideBox
+}
+
+
+
   public Start() {
-    this.setupXRCreateCube();
-  }
-
-
-
-  private setupXRCreateCube() {
     this.renderer.xr.enabled = true;
 
+    const geometry = new THREE.BoxGeometry(.3, .3, .3);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff * Math.random() });
+
     const self = this;
-    let controller: THREE.Group;
+
+    const insideBox = this.createInsideBox()
+    insideBox.scale.setScalar(300 * 0.99)
+
+    const outsideBox = this.createOutsideBox()
+    outsideBox.scale.setScalar(300)
+
+    this.scene.add(insideBox)
+    this.scene.add(outsideBox)
+
 
     function onSelect() {
-      const material = new THREE.MeshPhongMaterial({ color: 0xffffff * Math.random() });
-      const mesh = new THREE.Mesh(self.geometry, material);
-      mesh.position.set(0, 0, -0.3).applyMatrix4(controller.matrixWorld);
-      mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
-      self.scene.add(mesh);
-      self.meshes.push(mesh);
+      if (self.reticle.visible) {
+        const cube = new THREE.Mesh(geometry, material);
+        cube.position.setFromMatrixPosition(self.reticle.matrix);
+        cube.name = "cube"
+        self.scene.add(cube)
+      }
     }
 
-    document.body.appendChild(ARButton.createButton(this.renderer))
 
-    controller = this.renderer.xr.getController(0) as THREE.Group;
+
+    document.body.appendChild(ARButton.createButton(this.renderer, { requiredFeatures: ['hit-test'] }))
+
+    const controller: THREE.Group = this.renderer.xr.getController(0) as THREE.Group;
     controller.addEventListener('select', onSelect);
+
     this.scene.add(controller);
 
     this.renderer.setAnimationLoop(this.render.bind(this));
@@ -77,12 +161,45 @@ class App {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  private render() {
-    this.meshes.forEach((mesh) => {
-      mesh.rotateY(0.01);
-    });
+  //@ts-ignore
+  private render(timestamp: any, frame: any) {
+    if (frame) {
+      const referenceSpace = this.renderer.xr.getReferenceSpace();
+      const session = this.renderer.xr.getSession();
+
+      if (this.hitTestSourceRequested === false) {
+        session?.requestReferenceSpace('viewer').then(referenceSpace => {
+          //@ts-ignore
+          session?.requestHitTestSource({ space: referenceSpace })?.then(source =>
+            this.hitTestSource = source)
+        })
+
+        this.hitTestSourceRequested = true;
+
+        session?.addEventListener("end", () => {
+          this.hitTestSourceRequested = false;
+          this.hitTestSource = null;
+        })
+      }
+
+      if (this.hitTestSource) {
+        const hitTestResults = frame.getHitTestResults(this.hitTestSource);
+        if (hitTestResults.length > 0) {
+          const hit = hitTestResults[0];
+          this.reticle.visible = true;
+          this.reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix)
+
+        } else {
+          this.reticle.visible = false
+        }
+      }
+    }
+    this.scene.children.forEach(object => {
+      if (object.name === "cube") {
+        object.rotation.y += 0.01
+      }
+    })
     this.renderer.render(this.scene, this.camera);
   }
 }
-
-export { App } 
+export default App 
